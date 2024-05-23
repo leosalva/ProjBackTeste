f69344f28150520959463f8134e7552e0f612be9
leosalva <lssalvador89@gmail.com>
2024-05-22 21:22:39 -0300

    commit: DB configuration

--- ProjBackTeste/ProjBackTeste.API/Program.cs
+++ ProjBackTeste/ProjBackTeste.API/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using ProjBackTeste.Data.Context;
 using ProjBackTeste.IoC;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+#region "AddDbContext"
+var connectionStrings = builder.Configuration.GetConnectionString("DB");
+
+builder.Services.AddDbContext<ProjBackContext>(opt =>
+{
+    opt.UseNpgsql(connectionStrings);
+    opt.EnableSensitiveDataLogging(true);
+});
+
+#endregion
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ ProjBackTeste/ProjBackTeste.Data/Context/ProjBackContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjBackTeste.Data.Context
+{
+    public class ProjBackContext : DbContext
+    {
+        public ProjBackContext(DbContextOptions<ProjBackContext> option) : base(option) { }
+
+        //DBSets
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Class Maps
+        }
+    }
+}
